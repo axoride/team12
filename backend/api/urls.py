696791e046f9a96a428812f7806465ccd3b187c8
eling006 <eling006@fiu.edu>
2026-03-23 22:16:56 -0400

    Update urls.py
    Added route for moving books from wishlist to cart

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     # -------------------
     path('wishlists/create/', views.create_wishlist),
     path('wishlists/add-book/', views.add_book_to_wishlist),
+    path('wishlists/move-to-cart/', views.move_book_from_wishlist_to_cart),
 
     # -------------------
     # Book Browsing & Sorting
